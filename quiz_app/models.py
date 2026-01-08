4093d78285eb140cb15a62ad3efff3bd869ec10a
Prasuna-Bobba <prasuna3727@gmail.com>
2026-01-08 20:11:38 +0530

    Update models.py

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 
 class Quiz(models.Model):
+    title = models.CharField(max_length=255)
     url = models.URLField()
-    title = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
-class Question(models.Model):
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    question = models.TextField()
-    option_a = models.CharField(max_length=200)
-    option_b = models.CharField(max_length=200)
-    option_c = models.CharField(max_length=200)
-    option_d = models.CharField(max_length=200)
-    answer = models.CharField(max_length=1)
-    explanation = models.TextField()
+    def __str__(self):
+        return self.title
+
