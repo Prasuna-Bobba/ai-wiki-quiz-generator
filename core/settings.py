4c0b49a0bd032baea26dc9d321d891cc737061fc
Prasuna-Bobba <prasuna3727@gmail.com>
2026-01-08 11:35:42 +0530

    Fix formatting in settings.py

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'quiz_app',
 ]
